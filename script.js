9f02e6589a06adebbf3f9278e2794412ac01f404
Malak Markus <malak.markus@outlook.com>
2022-03-19 00:07:04 -0400

    add search form and more logic to JS

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
-var nameVar = document.querySelector(#idname);
-
-idname.addEventListener("click", idk);
-
-
-
-
-
-
-var getCitySearch = function () {
-    // format the github api url
-
-    var apiUrl = "https://api.teleport.org/api/cities/{?search} " + repo + "/issues?direction=asc";
-
-    // make a get request to url
-    fetch(apiUrl).then(function (response) {
-        // request was successful
-        if (response.ok) {
-            response.json().then(function (data) {
-                displayIssues(data);
-
-                // check if api has paginated issues
-                if (response.headers.get("Link")) {
-                    displayWarning(repo);
-                }
-            });
-        } else {
-            // if not successful, redirect to homepage
-            document.location.replace("./index.html");
-        }
-    });
+var cityFormEl = document.querySelector("#city-search");
+var cityInputEl = document.querySelector("#city-name");
+var resultsContainerEl = document.querySelector("#results-container");
+var citySearchTerm = document.querySelector("#city-search-term")
+// capture user input value
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    var cityName = cityInputEl.value.trim();
+    if (cityName) {
+        getCityInfo(cityName);
+        cityInputEl = "";
+    }
+    else {
+        alert("sorry! we don't have enough information about your city yet")
+    }
+}
+// city search function
+var  getCityInfo = function(city) {
+    var apiUrl = "https://api.teleport.org/api/cities/?search=" + city +""
+    fetch(apiUrl).then(function(response){ response.json().then(function(data){
+        displayCities(data, city)
+        console.log(data);
+        });
+    })
 };
+var displayCities = function(cities, searchTerm){
+    resultsContainerEl.textContent = "";
+    citySearchTerm.textContent = searchTerm;
+    console.log(cities);
+    console.log(searchTerm);
+    for (var i = 0; i < cities.length; i++){
+
+        resultDisplay = cities[i].search-results;
+        var cityEl = document.createElement("div");
+        cityEl.classList = "list-item";
+        var titleEl = document.createElement("span");
+        titleEl.textContent = resultDisplay;
+        cityEl.appendChild(titleEl);
+        resultsContainerEl.appendChild(cityEl);
+        
+    }
+
+
+};
+cityFormEl.addEventListener("submit", formSubmitHandler);
+    
