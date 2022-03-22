2b53d5e644490a235a2506d6cf736ec25f79e14b
Malak Markus <malak.markus@outlook.com>
2022-03-22 03:39:25 -0400

    add fetch, submithandler and display functions

--- script.js
+++ script.js
@@ -1,32 +1,111 @@
-var nameVar = document.querySelector(#idname);
+var searchFormEl = document.querySelector("#search-form");
+var inputEl = document.querySelector("#book-name");
+var resultsContainerEl = document.querySelector("#results-container");
 
-idname.addEventListener("click", idk);
-
-
-
-
-
-
-var getCitySearch = function () {
-    // format the github api url
-
-    var apiUrl = "https://api.teleport.org/api/cities/{?search} " + repo + "/issues?direction=asc";
-
-    // make a get request to url
-    fetch(apiUrl).then(function (response) {
-        // request was successful
-        if (response.ok) {
-            response.json().then(function (data) {
-                displayIssues(data);
-
-                // check if api has paginated issues
-                if (response.headers.get("Link")) {
-                    displayWarning(repo);
-                }
-            });
-        } else {
-            // if not successful, redirect to homepage
-            document.location.replace("./index.html");
-        }
-    });
+// search button logic
+var submitHandler = function(event){
+    event.preventDefault();
+    // console.log(event)
+    
+    // getting the value from the search form to use in the searchBooks function
+    var bookName = inputEl.value.trim();
+    if(bookName){
+        searchBooks(bookName);
+        inputEl.value = "";
+    }
+    else{
+        alert("please enter a book name")
+    }
 };
+
+// books search function
+var  searchBooks = function(book) {
+    
+    var apiUrl = "https://www.googleapis.com/books/v1/volumes?q=" + book + "";
+
+    fetch(apiUrl).then(function(response){
+    response.json().then(function(data){
+        displayBooksInfo(data, book)
+        //console.log(data);
+        });
+    });
+    
+};
+
+
+var displayBooksInfo = function(books) {
+    resultsContainerEl.textContent = "";
+    for (var i = 0; i < books.items.length; i++ ) {
+        
+        
+        //creat a container for each result
+        var resultEl = document.createElement("div");
+        resultEl.classList =  "list-inside";
+
+        //getting results for the book titles
+        var bookTitle = books.items[i].volumeInfo.title;
+
+        // creat a sapn for the title
+        var titleEl = document.createElement("span");
+        titleEl.textContent = bookTitle;
+
+        //console.log(bookTitle);
+
+        // append to the container
+        resultEl.appendChild(titleEl);
+
+        // getting results for authors names
+        var bookAuthors = books.items[i].volumeInfo.authors;
+
+        // creat a sapn for the authors
+        var authorsEl = document.createElement("span")
+        authorsEl.textContent = bookAuthors;
+        
+        // append to the container
+        resultEl.appendChild(authorsEl);
+
+
+        
+
+        
+        //getting results for book links
+        var bookLink = books.items[i].volumeInfo.previewLink;
+        //console.log(bookLink);
+
+        // creat a sapn for the link
+        var linkEl = document.createElement("link");
+
+        linkEl.textContent =bookLink;
+
+        // append to the container
+        resultEl.appendChild(linkEl);
+
+
+         //getting results for book images
+        var bookImg = books.items[i].volumeInfo.thumbnail;
+
+         // creat a sapn for the link
+        var imgEl = document.createElement("img");
+        imgEl.setAttribute("src", ""+bookImg+"");
+
+         // append to the container
+        resultEl.appendChild(imgEl);
+
+
+
+
+
+
+        //append container to the dom
+        resultsContainerEl.appendChild(resultEl);
+        // console.log(titleEl);
+
+
+
+    };
+    //console.log(books);
+};
+
+
+searchFormEl.addEventListener("submit", submitHandler);
+
