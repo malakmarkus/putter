de7890480de326be20ccdce7f16001b3cba03a80
kylieogg <kylieogg1@gmail.com>
2022-03-24 19:55:40 -0400

    changed line 3 of config file

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,16 +1,12 @@
 /* TAILWIND MEDIA QUERIES FOR SMALLER DESKTOP SCREENS AND SMALLER */
 
-module.exports = {
-    theme: {
-        screens: {
-            'sm': '640px',
-            // => @media (min-width: 640px) { ... }
-
-            'md': '768px',
-            // => @media (min-width: 1768px) { ... }
-
-            'lg': '1024px',
-            // => @media (min-width: 1024px) { ... }
-        },
-    }
-}
+export const theme = {
+    screens: {
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+        'md': '768px',
+        // => @media (min-width: 1768px) { ... }
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+    },
+};
